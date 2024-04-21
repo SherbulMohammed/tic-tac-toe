54b9f48a85c24a456152b1154f14b5502a60880f
Sherbul Mohammed <129470579+SherbulMohammed@users.noreply.github.com>
2024-04-21 14:26:04 +0100

    Add click event listeners to grid elements

--- script.js
+++ script.js
@@ -1,56 +1,9 @@
-const Gameboard = (() => {
-    let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-    ];
+const gridElements = document.querySelectorAll('[data-grid]')
 
-    const render = () => {
-        let boardHTML = "";
-        board.forEach((grid, index) => {
-            boardHTML += `<div class="grid" id="grid-${index}">${grid}</div>`
-        })
-        document.querySelector("#board").innerHTML = boardHTML;
-    }
-    
-
-    return {
-        render,
-    }
-
-})();
-
-const createPlayer = (name, mark) => {
-    return {
-        name,
-        mark
-    }
-}
-
-
-const Game = (() => {
-        let players = [];
-        let currentPlayerIndex = 0;
-        let gameOver = false;
-
-        const start = () => {
-            players = [
-                createPlayer(document.querySelector("#player1").value, "X"),
-                createPlayer(document.querySelector("#player1").value, "O")
-            ]
-            currentPlayerIndex = 0;
-            gameOver = false;
-            Gameboard.render();
-        }
-        return {
-            start,
-        }
-})();
-
-const startButton = document.querySelector("#startButton");
-startButton.addEventListener("click", () => {
-    //Game.start();
+gridElements.forEach(grid => {
+    grid.addEventListener('click', handleClick, { once: true })
 })
 
-  
-
+function handleClick(e) {
+    console.log('clicked')
+}
